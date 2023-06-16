9587cdf5783873543e07700e1082d9c42ed65ed4
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-15 18:35:12 -0700

    program with printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /*
  *first line tells program to include stdio libarary
-
-*main starting point of the program
-*using the main function this program prints "Programming is like building a multilingual puzzle
-*  Return: 0*/
+* main - print the string in the put function
+* *
+* * Description: using the main function
+* * this program prints "Programming is like building a multilingual puzzle
+* * Return: 0
+* */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
