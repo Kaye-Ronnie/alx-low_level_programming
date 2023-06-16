9edbc918763749502477f55ce7f4b275d998d0b5
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-15 18:23:13 -0700

    first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/*
+ *first line tells program to include stdio libarary
 
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
+*main starting point of the program
+*using the main function this program prints "Programming is like building a multilingual puzzle
+*  Return: 0*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);	
 }
