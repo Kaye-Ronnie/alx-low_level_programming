e07592a600bc8fde480cc97ac7bea2a3e7080086
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-19 03:06:55 -0700

    lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a' ; alphabet <= 'z' ;alphabet++)
+{
+	putchar(alphabet);
+}
+for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
+{
+	putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
