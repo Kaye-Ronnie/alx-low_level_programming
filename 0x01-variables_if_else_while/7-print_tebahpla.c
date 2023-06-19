0d96a73fcd9cda23e5abc80ac89536c3ec36858e
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-19 05:25:47 -0700

    reverse value

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+char rev_c;
+for (rev_c = 'z';rev_c >= 'a';rev_c--)
+{
+putchar(rev_c);
+}
+putchar('\n');
+return (0);
+}
