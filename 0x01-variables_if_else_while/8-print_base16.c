dd593feaf670f98e2cb78d412ff2ada732f9086e
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-19 05:57:36 -0700

    base sixteen

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,21 +3,21 @@
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- * this program prints "all numbers in base 16"
- * return: 0
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-	char num16;
+char num16;
 
-	for (num16 = '0'; num16 <= '9'; num16++)
-	{
-		putchar(num16);
-	}
-	for (num16 = 'a'; num16 <= 'f'; num16++)
-	{
-		putchar(num16);
-	}	
+for (num16 = '0'; num16 <= '9'; num16++)
+{
+	putchar(num16);
+}
+for (num16 = 'a'; num16 <= 'f'; num16++)
+{
+	putchar(num16);
+}	
 putchar('\n');
 return 0;
 }
