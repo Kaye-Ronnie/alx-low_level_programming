b32b11a5ed4f3980f5524539e6fc2ec28e75f02c
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-21 17:57:10 -0700

    nested trial

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** add two values
+ * num1: first value
+ * num2; second value
+ * return their sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * Print_to_98- prints all naturl numbers from n to 98
+ * @num: the number starts from
+ * return: always 0
+ */
+void print_to_98(int num)
+{
+	if (num <= 98)
+	{
+	for (; num <= 98; num++)
+	{
+		if (num == 98)
+		{
+			printf("%d", num);
+			printf("\n");
+			break;
+		}
+		else
+		{
+			printf("%d, ", num);
+		}
+	}
+	}
+	else
+	{
+		for (; num >= 98; num--)
+		{
+			if (num == 98)
+			{
+				printf("%d", num);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", num);
+			}
+		}
+	}
+}
