26be98297ffd49f81d04355c1137ff2a30ecde7b
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-27 05:38:10 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int strLength = 0;
+
+	while (s[strLength])
+	       strLength++;	
+	return (strLength);
+}
