46ca4883adddc1ccbd4a4fcaa88a13e9386f133c
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-29 15:45:30 -0700

    more pointer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * @n: number of buytes
+ * Return: pointer to destion string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count;
+
+	for (count = 0; count < n && src[count] != '\0'; count++)
+	dest[count] = src[count];
+
+	for (; count < n; count++)
+	dest[count] = '\0';
+	
+	return (dest);
+}
