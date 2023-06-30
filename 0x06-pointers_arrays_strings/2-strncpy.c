e455ae0ac9291d9772affc77b91470636e703df5
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-06-29 18:08:26 -0700

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int count;
+int count;
 
-	for (count = 0; count < n && src[count] != '\0'; count++)
-	dest[count] = src[count];
+for (count = 0; count < n && src[count] != '\0'; count++)
+dest[count] = src[count];
 
-	for (; count < n; count++)
-	dest[count] = '\0';
-	
-	return (dest);
+for (; count < n; count++)
+dest[count] = '\0';
+
+return (dest);
 }
