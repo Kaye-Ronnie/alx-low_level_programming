d64a7b918fc5e8f4df6a0f85d21ed2da8df684d1
Kaye-Ronnie <ronniwallace2017@gmail.com>
2023-07-05 18:51:48 -0700

    recursion exercis

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: parameter for checking length of a string
+ * Return: Always string length.
+ */
+int _strlen_recursion(char *s)
+{
+	int strlen =0;
+	if(*s)
+	{
+		strlen++;
+		strlen += _strlen_recursion(s + 1);
+	}
+	return (strlen);
+}
